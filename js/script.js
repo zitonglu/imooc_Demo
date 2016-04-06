681dff3faca8b46daa89859de1f55439f6ca650e
紫铜炉 <910109610@qq.com>
2016-04-06 22:00:24 +0800

    PadeIn
    第三页飞进

--- js/script.js
+++ js/script.js
@@ -37,9 +37,12 @@ window.onload = function(){
 	page1.addEventListener("touchstart",function(event){
 		page1.style.display="none";
 		page2.style.display="block";
+		page3.style.display="block";
+		page3.style.top="100%";
 
 		setTimeout(function() {
 			page2.setAttribute("class","page fadeOut");
+			page3.setAttribute("class","page fadeIn");
 		},5500);
 	},false);
 
